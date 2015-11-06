db2232696bb36aae704b58ad28c84030d93da49d
wadleo <wadingaleonard@gmail.com>
2015-11-06 05:45:16 +0100

    Add styling to most views especially to the base.html file

--- mcquizer/settings.py
+++ mcquizer/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for mcquizer project.
 
@@ -101,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import django.contrib.auth
 django.contrib.auth.LOGIN_URL = '/login/'
